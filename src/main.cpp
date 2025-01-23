be5098bd0cc228aa7da6aa155e746928c01110ac
obscurelyme <nico.frozencoffee@gmail.com>
2025-01-22 20:38:54 -0500

    assets

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,13 @@ void CleanUp() {
   Zero::CleanTraceLogSinks();
 }
 
+Color darkred{.r = 131, .g = 0, .b = 9, .a = 255};
+std::vector<Vector2> cachedMousePositions{};
+Vector2 currentVec2;
+
 int main() {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+  const int screenWidth = 1920;
+  const int screenHeight = 1080;
 
   Zero::InitTraceLogSinks();
   SetTraceLogCallback(Zero::RaylibTraceCallback);
@@ -22,16 +26,27 @@ int main() {
   InitWindow(screenWidth, screenHeight, "Basic Window");
   if (!IsWindowReady()) {
     Zero::ConsoleCrit("Raylib window was not initialized.");
-    Zero::CleanTraceLogSinks();
+    Zero::CleanTraceLogSinks();  // NOTE: Do not call CloseWindow because there is no window to close
     return Zero::Error::NoWindow;
   }
 
   SetTargetFPS(MAX_FRAMERATE);
 
   while (!WindowShouldClose()) {
+    // NOTE: input...
+    currentVec2 = GetMousePosition();
+    cachedMousePositions.push_back(currentVec2);
+
+    // NOTE: rendering...
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Congratz!", 190, 200, 20, BLACK);  // NOLINT
+    ClearBackground(BLACK);
+
+    for (const Vector2& vec2 : cachedMousePositions) {
+      DrawPixel(vec2.x, vec2.y, RED);
+    }
+
+    DrawLineEx(Vector2{0, 0}, Vector2{500, 500}, 10.0f, darkred);
+
     EndDrawing();
   }
 
